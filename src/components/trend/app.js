583c4e2b6c697654dc3c985b892891b495110eb4
roma <prowork1020@gmail.com>
2020-04-07 20:51:02 +0300

    create file trend and import his in an app

--- src/components/trend/app.js
+++ src/components/trend/app.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import Trend from './trend'
 export default class App extends React.Component{
     render(){
         return(
@@ -6,7 +7,9 @@ export default class App extends React.Component{
                 <h2>qwe</h2>
                 <h2>qwe</h2>
                 <div></div>
-                <div></div>
+                <div>
+                    <Trend/>
+                </div>
             </div>
         )
     }
---
+++ src/components/trend/trend.js
@@ -0,0 +1,14 @@
+import React from 'react'
+// import {link} from 'react-router-dom'
+
+export default class THrend extends React.Component{
+    constructor(props){
+        super(props)
+        this.state = {
+            trend: []
+        }
+    }
+    componentDidMount(){
+        fetch()
+    }
+}
